db2d71b5febe74511dc0a000ac5777ea2fe3c3a0
Venura-94 <venurapussella123@gmail.com>
2026-06-12 13:08:40 +0530

    added: student views and urls

--- student/views.py
+++ student/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+def add_student(request):
+    return render(request, 'students/add.html')
+
+def student_list(request):
+    return render(request, 'students/list.html')
+
+def edit_student(request):
+    return render(request, 'students/edit.html') 
+
+def view_student(request):
+    return render(request, 'students/view.html')
